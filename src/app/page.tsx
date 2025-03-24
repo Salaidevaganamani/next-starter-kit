70259c1f40936784825b32d3dcae861e24cfe35a
Salai Devaganamani <salai.devaganamani@outlook.com>
2025-03-24 11:55:52 +0530

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import db from '@/modules/db';
+import { faker } from '@faker-js/faker';
+import { revalidatePath } from 'next/cache';
+import Image from 'next/image';
+
+export default async function Home() {
+  const posts = await db.post.findMany({ orderBy: { createdAt: 'desc' } });
+
+  const generatePosts = async () => {
+    'use server';
+
+    await db.post.createMany({
+      data: [
+        { content: faker.lorem.sentence() },
+        { content: faker.lorem.sentence() },
+        { content: faker.lorem.sentence() },
+      ],
+    });
+    revalidatePath('/');
+  };
+
+  return (
+    <main className="flex flex-col min-h-screen p-24">
+      <button onClick={generatePosts}>Generate Posts</button>
+      {posts.map((post) => (
+        <div key={post.id}>{post.content}</div>
+      ))}
+    </main>
+  );
+}
